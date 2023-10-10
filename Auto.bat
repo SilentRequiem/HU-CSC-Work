6296a5bd20fa175fdcdbef8aa108c3cf211e57f5
SilentRequiem <66715677+SilentRequiem@users.noreply.github.com>
2023-10-10 14:19:09 -0400

    Update Auto.bat

--- Auto.bat
+++ Auto.bat
@@ -1,3 +1,9 @@
-              - name: Git Auto Commit
-                uses: https://github.com/SilentRequiem/HU-CSC-Work
-            
+pushd "%userprofile%\OneDrive\Documents\GitHub\HU-CSC-Work\CSC 151 02"
+echo.>.gitignore
+bash -c "command find . -type f -size +99M >> .gitignore"
+bash -c "sed -i "s,^\./,," .gitignore" 
+git add --all
+git commit -m "autoCommit %date:~-4%%date:~3,2%%date:~0,2%.%time:~0,2%%time:~3,2%%time:~6,2%"
+git push
+popd
+exit
