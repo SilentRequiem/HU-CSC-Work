e918e8ded7f54ddd0d57da96716c42d067532fca
SilentRequiem <66715677+SilentRequiem@users.noreply.github.com>
2025-02-18 13:04:07 -0500

    2/18/25

---
+++ CSC 152 Edits 2/Extra Assignments/Program Assignment 2 - Class and Object/AmooM_P2.java
@@ -0,0 +1,25 @@
+public class Triangle {
+
+    //Attributes
+    private int base;
+    private int height;
+    private String color;
+
+public Triangle() { //no args constructor (defaults)
+    base = 96; //student ID
+    height = 22; //day of birth day
+    color = "February"; //Birth month name
+    }
+
+    public Triangle(int aBase; int aHeight; String aColor) {  //Parameter constructor (this.blah)
+    this.base = aBase; 
+    this.height = aHeight;
+    this.color = aColor;
+    }
+
+    public void changeBase() { //mutators methods
+    this.base = newBase;
+    }
+
+
+}
