c5ba5851641406fafa62941ec1e2ae6bc8714d2b
Quietninja0954 <michaelamoo54@gmail.com>
2026-09-08 11:17:09 -0400

    9/8/26

---
+++ CSC 251 01/Algorithm Analysis_Review Arrays/ArrayReview3/Array.java
@@ -0,0 +1,16 @@
+
+
+/*
+
+1) arrayStr = size = 15
+    store 10 strings
+
+
+2) only store strings with 1st char is H
+
+3) Search array for how many occurences of: str
+    return numbers of occurences
+
+4) search array for 
+
+*/
