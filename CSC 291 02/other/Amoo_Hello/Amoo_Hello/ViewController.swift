fbc4c718af36e7959010eb934b4e14c00f19b7f2
SilentRequiem <66715677+SilentRequiem@users.noreply.github.com>
2026-09-07 23:51:36 -0400

    9/7/26

---
+++ CSC 291 02/other/Amoo_Hello/Amoo_Hello/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Amoo_Hello
+//
+//  Created by Michael Amoo on 3/9/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
