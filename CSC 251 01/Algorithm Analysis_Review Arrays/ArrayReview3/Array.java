d2acad41ccaaf5e0d31fc206e259cc2e9f081f09
Quietninja0954 <michaelamoo54@gmail.com>
2026-09-08 21:33:34 -0400

    Update Array.java

--- CSC 251 01/Algorithm Analysis_Review Arrays/ArrayReview3/Array.java
+++ CSC 251 01/Algorithm Analysis_Review Arrays/ArrayReview3/Array.java
@@ -1,4 +1,4 @@
-
+import java.util.Scanner;
 
 /*
 
@@ -11,6 +11,121 @@
 3) Search array for how many occurences of: str
     return numbers of occurences
 
-4) search array for 
+4) search array for longest string return longest string
 
 */
+
+public class Array {
+    private int size = 15;
+    private String[] arrayStr = new String[size];
+    private int count;
+
+    Scanner keyboard = new Scanner(System.in);
+
+    public void store10() {
+        count = 0;
+
+        for(int i = 0; i < 10; i++) {
+
+            System.out.println("Enter a name: ");
+            arrayStr[i] = keyboard.nextLine();
+            count++;
+        }
+
+        System.out.println("Inputed" + count + "times.");
+    }
+
+    public void store1stH() {
+        String word;
+        count = 0;
+
+        for (int i = 0; i < 10; i++) {
+            
+            System.out.println("Enter only a word that begins with a capital H: ");
+            word = keyboard.nextLine();
+
+            while(word.charAt(0) != 'H') {
+                System.out.println("ONLY enter words that begin in CAPITAL H: ");
+                System.out.println("Enter a word: ");
+                word = keyboard.nextLine();
+            }
+
+            arrayStr[i] = word;
+            count++;
+        }
+    }
+
+
+    public int search(String str){
+        int strCount = 0;
+
+        for(int i = 0; i < count; i++) {
+            
+            if(arrayStr[i].equals(str)){
+                strCount++;
+            }
+
+        }
+        
+        return strCount;
+    }
+
+    public int longest() {
+        int longest = arrayStr[0].length();
+
+        for (int i = 0; i < count; i++) {
+            
+            if(arrayStr[i].length() > longest){
+                longest = arrayStr[i].length();
+            }
+
+        }
+        return longest;
+    }
+
+
+    public double average() {
+        int[] numArray = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int sum = 0;
+
+        for (int i = 0; i < 10; i++) {
+            sum += numArray[i];
+        }
+
+        double avg = sum / 10;
+        return avg;
+
+    }
+
+
+    public boolean find(int x) {
+        int[] numArray2 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        for (int i = 0; i < 10; i++) {
+            if (numArray2[i] == x){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void findRange() {
+        int[] numArray3 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        for (int i = 0; i < 10; i++) {
+            
+            if (numArray3[i] > 10 && numArray3[i] < 160) {
+                
+                System.out.print(numArray3[i] + ", ");
+            }
+        }
+
+    }
+
+
+
+    public static void main(String[] args) {
+        
+        
+
+
+    }
+}
